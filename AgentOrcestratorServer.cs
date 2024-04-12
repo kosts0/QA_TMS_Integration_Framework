827c0924922deb60ea88550d26d643986ef37fff
kosts0 <skld0@bk.ru>
2024-04-12 21:19:18 +0700

    Реализация с SignalR

---
+++ AgentOrcestratorServer.cs
@@ -0,0 +1,83 @@
+﻿using CommonLibrary;
+using Confluent.Kafka;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json.Linq;
+using WebSocketSharp;
+namespace TestRunnerKafkaConsumer
+{
+    public class AgentOrcestratorServer : Hub
+    {
+        private readonly IConsumer<Ignore, string> _consumer;
+        private readonly ILogger<AgentOrcestratorServer> _logger;
+        WebSocketBehavior eh;
+        RunnerConfig RunnerConfig = new();
+        public AgentOrcestratorServer(IConfiguration configuration, ILogger<AgentOrcestratorServer> logger)
+        {
+            _logger = logger;
+            var consumerConfig = new ConsumerConfig
+            {
+                BootstrapServers = configuration["Kafka:BootstrapServers"],
+                GroupId = "AutoTestConsumer",
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+            };
+            configuration.GetSection("RunnerConfig").Bind(RunnerConfig);
+            _consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build();
+            _consumer.Subscribe("AutoTestTopic");
+            _logger.LogInformation("SignalR Hub подключен к топику AutoTestTopic!");
+            ProcessKafkaMessage();
+        }
+        public async void ProcessKafkaMessage()
+        {
+            try
+            {
+                _logger.LogInformation("Before staring to consume!");
+                while (true)
+                {
+                    var consumeResult = _consumer.Consume();
+                    var message = consumeResult.Message.Value;
+                    _logger.LogInformation($"Получено сообщение из топика kafka о запуске автотеста: {message}");
+                    this.RunTest(JToken.Parse(message)).Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Ошибка обработки сообщения из kafka: {ex.Message}");
+            }
+        }
+        public List<string> ConnectionIdList { get; set; } = new();
+        private List<string> InProcessConnecions { get; set; } = new();
+        public async Task Send(string message)
+        {
+            await this.Clients.All.SendAsync("Send", message);
+        }
+        public override Task OnConnectedAsync()
+        {
+            var connectionId = this.Context.ConnectionId;
+            ConnectionIdList.Add(connectionId);
+            _logger.LogInformation($"Подключен тест-раннер {connectionId}");
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            var connectionId = this.Context.ConnectionId;
+            ConnectionIdList.Remove(connectionId);
+            _logger.LogWarning($"Отключен тест-раннер {connectionId}");
+            return base.OnDisconnectedAsync(exception);
+        }
+        public async Task RunTest(JToken runCommand)
+        {
+            var connectionId = ConnectionIdList.FirstOrDefault(c => !InProcessConnecions.Contains(c));
+            while(connectionId == null)
+            {
+                connectionId = ConnectionIdList.FirstOrDefault(c => !InProcessConnecions.Contains(c));
+            }
+            InProcessConnecions.Add(connectionId);
+            _logger.LogInformation($"Запуск теста на раннере {connectionId}");
+            await this.Clients.Client(connectionId).SendAsync("RunTest", runCommand);
+        }
+        public async Task TestFinished(string message)
+        {
+            _logger.LogInformation($"Test Finished from {Context.ConnectionId} {DateTime.Now}, {message}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,74 +1,26 @@
-using Microsoft.AspNetCore.Server.Kestrel.Core;
-using Newtonsoft.Json.Linq;
-using System.Text;
-using System.Text.Json;
-using TestItAdapter;
-using TestProject;
-using TestRunnerWebApi;
-using TestRunnerWebApi.TestItAdapter;
+using CommonLibrary;
+using TestRunnerKafkaConsumer;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.Configure<KestrelServerOptions>(options =>
-{
-    options.AllowSynchronousIO = true;
-});
-builder.Services.Configure<IISServerOptions>(options =>
-{
-    options.AllowSynchronousIO = true;
-});
 builder.Configuration.AddJsonFile("config.json");
+builder.Services.AddSignalR().AddHubOptions<AgentOrcestratorServer>(options =>
+{
+    options.EnableDetailedErrors = true;
+    options.KeepAliveInterval = TimeSpan.FromSeconds(5);
+});
 var app = builder.Build();
+
+app.UseRouting();
+app.UseDefaultFiles();
+app.MapHub<AgentOrcestratorServer>("/chat");
+
+
 
 Console.WriteLine("Starting init TestItApi");
 TestItApi.Init(GetConfig(builder.Configuration));
 Console.WriteLine("TestItApi init sucess");
 
-// Configure the HTTP request pipeline.
-//app.UseHttpsRedirection();
 
-app.MapPut("/runTest", (HttpRequest request, IConfiguration appConfig) =>
-{
-    var requestContent = "";
-    request.EnableBuffering();
-    using (var reader = new StreamReader(request.Body, Encoding.UTF8, true, 1024, true))
-    {
-        requestContent = reader.ReadToEnd();
-    }
-    request.Body.Position = 0;
-
-    JToken requestBody = JToken.Parse(requestContent);
-
-    Guid testRunId = Guid.Parse(requestBody["TestRunId"]?.ToString());
-    List<string> autotestGlobalIdList = requestBody.SelectTokens("AutoTests[*].GlobalId").Select(t => t.ToString()).ToList();
-    RunnerConfig runnerConfig = GetConfig(appConfig);
-    try
-    {
-        foreach(var testId in autotestGlobalIdList)
-        {
-            string testRunReportFolder = Guid.NewGuid().ToString();
-            Console.WriteLine($"Saving test result for test {testId} in {testRunReportFolder}");
-            var manualTestId = TestItApi.AutoTestsApi.GetWorkItemsLinkedToAutoTest(testId).First().GlobalId;
-            Console.WriteLine($"Starting Test {manualTestId}");
-            CmdHelper.ExecuteCommand($"dotnet test {Path.GetFileName(runnerConfig.DllPath)} -v n --filter \"Name~{manualTestId}\" -- TestRunParameters.Parameter(name=\\\"TestRunId\\\", value=\\\"{testRunReportFolder}\\\")", workingDirectory: Path.GetDirectoryName(GetConfig(appConfig).DllPath));
-            Console.WriteLine($"TestExecute Completed");
-            CmdHelper.ExecuteCommand(new ImportCommand(GetConfig(appConfig)) { TestResultDirectory = @$"{Path.GetDirectoryName(GetConfig(appConfig).DllPath)}\allure-results\{testRunReportFolder}", TestRunId = testRunId }.ProcessInfo);
-            Console.WriteLine($"testRunned {DateTime.Now}\n");
-        }
-        
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"TestExec failed {ex.Message}");
-    } 
-    return;
-});
-app.MapGet("/", (IConfiguration appConfig) =>
-{
-    RunnerConfig config = GetConfig(appConfig);
-    return JsonSerializer.Serialize<RunnerConfig>(config);
-});
 app.Run();
 
 
