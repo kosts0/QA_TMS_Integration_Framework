cfd460be1c1bf0d8028f81304e35dd007f767195
kosts0 <skld0@bk.ru>
2024-05-10 14:44:58 +0700

    объединение проектов в директории

---
+++ HookKafkaProducer/ProducerService.cs
@@ -0,0 +1,34 @@
+﻿using Confluent.Kafka;
+
+namespace HookKafkaProducer
+{
+    public class ProducerService
+    {
+        private readonly IConfiguration _configuration;
+
+        private readonly IProducer<string, string> _producer;
+        private readonly ILogger<ProducerService> _logger;
+
+        public ProducerService(IConfiguration configuration, ILogger<ProducerService> logger)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            
+            string bootstrapServers = _configuration["Kafka:BootstrapServers"];
+            _logger.LogInformation($"Starting connect to Kafka Bootstrap servers {bootstrapServers}");
+            var producerconfig = new ProducerConfig
+            {
+                BootstrapServers = bootstrapServers
+            };
+            _producer = new ProducerBuilder<string, string>(producerconfig).Build();
+            _logger.LogInformation("Connection to Bootstrap servers finished");
+        }
+
+        public async Task ProduceAsync(string topic, string message, string key = null)
+        {
+            var kafkamessage = new Message<string, string> { Value = message, Key = Guid.NewGuid().ToString() };
+
+            await _producer.ProduceAsync(topic, kafkamessage);
+        }
+    }
+}
